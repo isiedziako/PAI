c94995b3273f381c7306e285f6560a5eff618432
isiedziako <siedziakoi@gmail.com>
2022-09-26 14:53:57 +0200

    Komentarz

---
+++ Lekcja 3/script.js
@@ -0,0 +1,26 @@
+liczba = 2
+while(liczba <= 5)
+{
+    document.write(liczba + "<br>")
+    liczba++
+}
+
+for(liczba = 12; liczba <= 16; liczba++)
+{
+    document.write(liczba + "<br>")  
+document.write("<br>") 
+}
+liczba = 4
+while(liczba <= 12)
+{
+    if(liczba % 2 == 0)
+    {
+        document.write(liczba + "<br>")
+     }
+     liczba++
+}
+
+for(liczba = 0; liczba < 4; liczba++)
+{
+     document.write("Liczba <br>") 
+}
